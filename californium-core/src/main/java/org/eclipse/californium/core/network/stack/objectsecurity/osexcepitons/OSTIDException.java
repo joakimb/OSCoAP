d624d874a1cd15c957ba706a4c688e9489ea2b56
Joakim Brorsson <b.joakim@gmail.com>
2016-03-10 16:29:13 +0100

    ObjectSecurity now selected as an attribute in CoapClient

---
+++ californium-core/src/main/java/org/eclipse/californium/core/network/stack/objectsecurity/osexcepitons/OSTIDException.java
@@ -0,0 +1,11 @@
+package org.eclipse.californium.core.network.stack.objectsecurity.osexcepitons;
+
+/**
+ * Created by joakim on 2016-03-10.
+ */
+public class OSTIDException extends Exception{
+    public OSTIDException(String message) {
+        super(message);
+    }
+
+}
