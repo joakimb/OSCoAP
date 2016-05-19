0269038f10b3991ce4621d107755506c4f75b869
Joakim Brorsson <b.joakim@gmail.com>
2016-05-19 16:00:56 +0200

    m

--- californium-core/src/main/java/org/eclipse/californium/core/network/serialization/DataParser.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/serialization/DataParser.java
@@ -55,9 +55,21 @@ public class DataParser {
 	private int mid;
 	
 	public DataParser(byte[] bytes) {
+
+		System.out.println("RECEIVING BYTES: " + bytesToHex(bytes));
 		setBytes(bytes);
 	}
-	
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
 	public void setBytes(byte[] bytes) {
 		this.reader = new DatagramReader(bytes);
 		this.version = reader.read(VERSION_BITS);
